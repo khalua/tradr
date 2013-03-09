5bbb376f8ad7a70a5a86da073a9ec75f44368d56
Tony Contreras <tony.contreras@gmail.com>
2013-03-09 10:19:32 -0500

    moving to home pc

--- app/assets/javascripts/tradr.js
+++ app/assets/javascripts/tradr.js
@@ -1,6 +1,7 @@
 $(function() {
   $('#close_user_form_button').click(close_new_user_form);
   $('#close_stock_form_button').click(close_buy_stock_form);
+  $('.box').click(show_graph);
 });
 
 function close_new_user_form()
@@ -16,4 +17,28 @@ function close_buy_stock_form()
 function hide_buy_stock_button()
 {
   $('#show_new_stock_button').hide();
+}
+
+function show_graph ()
+{
+  var symbol = $(this).children().attr('name');
+  console.log(symbol);
+  get_data();
+
+}
+
+funtion get_data()
+{
+
+  var id = $(this).parent().siblings('.clear').prev().text();
+  var token = $('input[name=authenticity_token]').val();
+
+  $.ajax({
+    dataType: 'json',
+    type: "post",
+    url: "/priorities/" + id + "/up",
+    data: {authenticity_token:token}
+  }).done(process_priority);
+}
+
 }
